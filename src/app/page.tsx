8deb102755b00274a03c8c817e808dfd83926238
monysary <sary.mony@gmail.com>
2025-10-06 08:18:29 -0700

    established db connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import { db } from "@/db";
+
 export default function Home() {
+  console.log(db);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
